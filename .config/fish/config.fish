da4e144d86306010569bffe5cc9a242a69e19d23
typewriter <takuyaxp@gmail.com>
2020-01-12 14:39:02 +0900

    Add peco functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,24 @@ function cd
     builtin cd $argv; and ls
 end
 
+function pecovi
+  peco | awk -F: '{printf $1}' | read selected
+  if [ $selected ]
+    vim $selected
+  end
+end
+
+function glsvi
+  git ls-files | peco --query "$LBUFFER" | read selected
+  if [ $selected ]
+    vim $selected
+  end
+end
+
 function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-  bind \c] peco_select_ghq_repository
+  bind -M insert \cr 'peco_select_history (commandline -b)'
+  # bind \c] peco_select_ghq_repository
+  bind -M insert \cg glsvi
 end
 
 set -g theme_show_exit_status no
